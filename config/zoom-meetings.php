aac150b9c449df6d0112926946b8c4dfc32015bd
yurii <0969966@gmail.com>
2023-05-05 12:38:19 +0300

    feat: add refresh and revoke token functions

--- config/zoom-meetings.php
+++ config/zoom-meetings.php
@@ -6,4 +6,5 @@ return [
     'client_secret' => env('ZOOM_CLIENT_SECRET'),
     'base_url' => 'https://api.zoom.us/v2/',
     'token_url' => 'https://zoom.us/oauth/token',
+    'revoke_url' => 'https://zoom.us/oauth/revoke',
 ];
--- src/Support/Auth.php
+++ src/Support/Auth.php
@@ -14,17 +14,17 @@ class Auth
      */
     public static function getToken(): string
     {
-        $account_id = config('zoom-meetings.account_id');
-        $client_id = config('zoom-meetings.client_id');
-        $client_secret = config('zoom-meetings.client_secret');
+        $accountId = config('zoom-meetings.account_id');
+        $clientId = config('zoom-meetings.client_id');
+        $clientSecret = config('zoom-meetings.client_secret');
 
-        if (!$account_id || !$client_id || !$client_secret) {
+        if (!$accountId || !$clientId || !$clientSecret) {
             throw new MissingConfigException('Zoom account_id, client_id and client_secret must be set in config/zoom.php');
         }
 
         $client = new Client([
             'headers' => [
-                'Authorization' => 'Basic ' . base64_encode($client_id . ':' . $client_secret),
+                'Authorization' => 'Basic ' . base64_encode($clientId . ':' . $clientSecret),
                 'Host' => 'zoom.us',
             ],
         ]);
@@ -32,12 +32,83 @@ class Auth
         $response = $client->request('POST', config('zoom-meetings.token_url'), [
             'form_params' => [
                 'grant_type' => 'account_credentials',
-                'account_id' => $account_id,
+                'account_id' => $accountId,
             ],
         ]);
 
         $responseBody = json_decode($response->getBody(), true);
 
         return $responseBody['access_token'];
+    }
+
+    /**
+     * @param string $refreshToken
+     * @return array
+     * @throws GuzzleException
+     * @throws MissingConfigException
+     */
+    public static function refreshToken(string $refreshToken): array
+    {
+        $clientId = config('zoom-meetings.client_id');
+        $clientSecret = config('zoom-meetings.client_secret');
+
+        if (!$clientId || !$clientSecret) {
+            throw new MissingConfigException('Zoom client_id and client_secret must be set in config/zoom.php');
+        }
+
+        $options = [
+            'headers' => [
+                'Authorization' => 'Basic ' . base64_encode($clientId . ':' . $clientSecret),
+                'Content-Type' => 'application/json',
+                'Accept' => 'application/json',
+            ],
+        ];
+
+        $params = [
+            'grant_type' => 'refresh_token',
+            'refresh_token' => $refreshToken
+        ];
+
+        $response = (new Client($options))->post(config('zoom-meetings.token_url'), [
+            'form_params' => $params
+        ]);
+
+        return json_decode($response->getBody(), true);
+    }
+
+    /**
+     * @param string $accessToken
+     * @return bool
+     * @throws GuzzleException
+     * @throws MissingConfigException
+     */
+    public static function revokeToken(string $accessToken): bool
+    {
+        $clientId = config('zoom-meetings.client_id');
+        $clientSecret = config('zoom-meetings.client_secret');
+
+        if (!$clientId || !$clientSecret) {
+            throw new MissingConfigException('Zoom client_id and client_secret must be set in config/zoom.php');
+        }
+
+        $options = [
+            'headers' => [
+                'Authorization' => 'Basic ' . base64_encode($clientId . ':' . $clientSecret),
+                'Content-Type' => 'application/json',
+                'Accept' => 'application/json',
+            ],
+        ];
+
+        $params = [
+            'token' => $accessToken,
+        ];
+
+        $response = (new Client($options))->post(config('zoom-meetings.revoke_url'), [
+            'form_params' => $params
+        ]);
+
+        $responseBody = json_decode($response->getBody(), true);
+
+        return ($responseBody['status'] ?? '') == 'success';
     }
 }
